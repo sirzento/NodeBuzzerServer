4b07e06435b7244eea49197a200abf4d7895134a
Nico <sirzento@gmx.de>
2019-06-28 23:06:52 +0200

    zu viel neues

--- buzzer.js
+++ buzzer.js
@@ -6,7 +6,8 @@ var io = require('socket.io')(http);
 //Globale Variable
 var BuzzerStatus = true;
 var Playerlist = [];
-var CurrentPress = {};
+var CurrentPress = new Object();
+var CurrentQuestion = "";
 
 //Weiterleitung zur Buzzer Page
 app.get('/buzzer', function (req, res) {
@@ -28,27 +29,57 @@ app.get('/admin', function (req, res) {
 io.on('connection', function (socket) {
 
     //Schickt beim erstmaligen laden der Seite die Info des Spieler, der aktuell gedrückt hat, falls vorhanden
-    socket.emit('load buzzer', CurrentPress.Color);
+    socket.emit('update ranking', Playerlist);
+    socket.emit('load buzzer', CurrentPress);
+    socket.emit('set question', CurrentQuestion);
 
     //Logik wenn der Buzzer gedrückt worden ist
     socket.on('buzzer press', function (msg) {
         if (BuzzerStatus == true) {
             BuzzerStatus = false;
+            CurrentPress = Playerlist.find(function (element) { return element.SocketID == socket.id });
+            console.log(CurrentPress.Name);
             //Nachricht an den Spieler, der Erfolgreich gedrückt hat
             socket.emit('successful pressed', "");
             //Nachricht an ale anderen das jemand gedrückt hat
             socket.broadcast.emit('failed pressed', "");
-            CurrentPress = Playerlist.find(function (element) { return element.SocketID == socket.id });
             //Schickt Info des Spielers der erfolgreich gedrückt hat an alle Spieler
-            io.emit('buzzer press', CurrentPress.Color);
+            io.emit('buzzer press', CurrentPress);
         }
     });
 
     //Schaltet den Buzzer wieder frei
     socket.on('buzzer unlocked', function (msg) {
         BuzzerStatus = true;
+        io.emit('buzzer unlocked', CurrentPress);
         CurrentPress = new Object();
-        io.emit('buzzer unlocked', "");
+    });
+
+    //Löscht die aktuelle frage
+    socket.on('clear question', function (msg) {
+        CurrentQuestion = "";
+        io.emit('clear question', "");
+    });
+
+    //Sendet eine neue Frage zu den Clienten
+    socket.on('send question', function (msg) {
+        CurrentQuestion = msg;
+        io.emit('set question', msg);
+    });
+
+    //Startet ein neues Spiel
+    socket.on('new game', function (msg) {
+        BuzzerStatus = true;
+        Playerlist = [];
+        CurrentPress = new Object();
+        CurrentQuestion = "";
+        io.emit('new game', msg);
+    });
+
+    //fügt Punkte zum aktuellem Clienten hinzu
+    socket.on('add points', function (msg) {
+        Playerlist.find(function (element) { return element.SocketID == CurrentPress.SocketID }).Points += parseInt(msg);
+        UpdateRanking();
     });
 
     //Logik wenn ein neue Spieler sich einträgt
@@ -60,11 +91,13 @@ io.on('connection', function (socket) {
 
     //Logik falls ein Spieler die Seite verlässt
     socket.on('disconnect', function () {
-        console.log('User ' + Playerlist.find(function (element) { return element.SocketID == socket.id }).Name + ' disconnected.');
-        Playerlist.splice(Playerlist.findIndex(myArray, function (item) {
-            return item.SocketID == socket.id;
-        }), 1);
-        UpdateRanking();
+        if (Playerlist.find(function (element) { return element.SocketID == socket.id }) != null) {
+            console.log('User ' + Playerlist.find(function (element) { return element.SocketID == socket.id }).Name + ' disconnected. [' + socket.id + ']');
+            Playerlist = Playerlist.filter(function (obj) {
+                return obj.SocketID != socket.id;
+            });
+            UpdateRanking();
+        }
     });
 });
 
@@ -92,13 +125,16 @@ function NewPlayer(name, color, socketID) {
 
 //Sortiert die Spielerliste/Rangliste nach Punkten und sendet sie zu allen Clienten/Spieler
 function UpdateRanking() {
-    Playerlist.sort((a, b) => (a.Points > b.Points) ? 1 : ((b.Points > a.Points) ? -1 : 0));
+    Playerlist.sort((a, b) => (a.Points < b.Points) ? 1 : ((b.Points < a.Points) ? -1 : 0));
     io.emit('update ranking', Playerlist);
+    if (JSON.stringify(CurrentPress) != JSON.stringify(new Object())) {
+        io.emit('buzzer press', CurrentPress);
+    }
 }
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//// Load Sound Files
+//// Load Resource Files
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 app.get('/sounds/Game_Over_Robot_Hit_8.mp3', function (req, res) {
